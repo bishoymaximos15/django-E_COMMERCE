5539c759476c7ace9b26134c616bae6f1ca4b4c2
Pythondeveloper6 <pythondeveloper6@gmail.com>
2022-12-30 14:42:52 +0200

    create url for products app

--- project/urls.py
+++ project/urls.py
@@ -14,16 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from products.views import ProductsList , ProductsDetail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('products/',ProductsList.as_view()),
-    path('products/<int:pk>/',ProductsDetail.as_view()),
+    path('products/', include('products.urls'))
 
 ]
 
