068ea03c4cacc8dd7275a5a837339f3897728081
Pythondeveloper6 <pythondeveloper6@gmail.com>
2023-01-06 14:33:39 +0200

    add url names , edit base template

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from .views import ProductsList , ProductsDetail
 
 
+app_name='products'
+
 urlpatterns = [
-    path('',ProductsList.as_view()),
+    path('',ProductsList.as_view(),name='product_list'),
     path('<slug:slug>/',ProductsDetail.as_view()),
 ]
